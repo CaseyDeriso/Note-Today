26789b2d10b88cea516d005894cd032216228d87
Casey <caseyderiso@gmail.com>
2020-12-11 17:00:59 -0600

    finished up post request and added decoding of incoming data in server.js

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -4,13 +4,13 @@ const { notes } = require("../../data/notes")
 
 router.get("/notes", (req, res) => {
   let results = notes;
-  if (req.query) {
-    console.log("query")
-  }
   res.json(results);
 })
 
 router.post("/notes", (req,res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString()
+
   if(!validateNote(req.body)) {
     res.status(400).send("The note is not properly formatted.")
   } else {
@@ -18,5 +18,7 @@ router.post("/notes", (req,res) => {
     res.json(note)
   }
 })
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoing JSON data
+app.use(express.json());
+// link app to routes for api
 app.use("/api", apiRoutes);
+// link app to routes for html
 app.use('/', htmlRoutes);
 // set up path to public  directory
 app.use(express.static("Public"))
